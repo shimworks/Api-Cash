357c41957c6c95a26373c76c2dd7ca70439e5ccf
shimworks <shimworks.tk@gmail.com>
2025-07-22 15:59:50 -0300

    Corrige .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: process.env.DOCKER_MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
