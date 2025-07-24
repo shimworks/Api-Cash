ec944606a7f1e11214af55438b91f9254e1f8047
shimworks <shimworks.tk@gmail.com>
2025-07-24 09:43:22 -0300

    Padroniza .env

--- src/server.js
+++ src/server.js
@@ -6,7 +6,4 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
-  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
-  console.log(process.env.BATATOSO)
-
 });
